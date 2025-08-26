d0c4cf8ab64054968c3b69bfb1bb2348611112b8
Allaith <112009allaith@gmail.com>
2025-08-26 11:37:23 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,17 @@
+'use client';
+
+import { initializeApp, getApps, getApp } from 'firebase/app';
+
+const firebaseConfig = {
+  projectId: "talween-studio",
+  appId: "1:378767438301:web:5b5f6a66e78b4323668634",
+  storageBucket: "talween-studio.firebasestorage.app",
+  apiKey: "AIzaSyB0TxPiCvTevv9k-mLdzocEIvRs9bo_loE",
+  authDomain: "talween-studio.firebaseapp.com",
+  messagingSenderId: "378767438301"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+export default app;
